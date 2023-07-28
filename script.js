05d8429fdf0ebbd00075335ddd74304fe0cde2e4
Anastasia Collins <anastasiacollins@Anastasias-MacBook-Air.local>
2023-07-27 20:34:31 -0500

    alerts for form not filled out

--- script.js
+++ script.js
@@ -6,7 +6,37 @@ window.addEventListener("load", function() {
 document.getElementById('formSubmit').addEventListener('click', function(event){
     event.preventDefault();
     //test button to see how it is working
-    confirm("All fields are required");
+   // confirm("All fields are required");
+    
+        const nameInput = document.getElementById('pilotName');
+        const copilotName = document.getElementById('copilotName');
+        const fuelInput = document.getElementById('fuelLevel');
+        const cargoInput = document.getElementById('cargoMass');
+    
+        // Checking to see if the form is blank
+        if (!nameInput || !copilotName || !fuelInput || !cargoInput) {
+            alert('Please fill in all fields.');
+            return;
+        }
+          
+         // Validating names to contain only letters and spaces
+        if (!/^[a-zA-Z ]+$/.test(nameInput)) {
+            alert('Invalid response. Please enter text for pilot.');
+            return;
+        }
+          
+        if (!/^[a-zA-Z ]+$/.test(copilotName)) {
+            alert('Invalid response. Please enter text for copilot.');
+            return;
+        }
+          
+        // Validating fuel and cargo input to be numbers
+        if (isNaN(fuelInput) || isNaN(cargoInput)) {
+            alert('Invalid response, please enter a number for fuel and cargo.');
+            return;
+        }
+    
+        confirm('Form Successful!');
 });
 
 
